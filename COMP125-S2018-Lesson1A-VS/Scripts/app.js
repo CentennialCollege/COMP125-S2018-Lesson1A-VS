30b43a18ea3b730132ae696b06495e3337ea8afa
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-05-09 11:17:06 -0400

    Initial Commit

---
+++ COMP125-S2018-Lesson1A-VS/Scripts/app.js
@@ -0,0 +1,11 @@
+﻿// IIFE - Immediately Invoked Function Expression
+(function () {
+    function Start() {
+        console.log("App Started");
+        console.info("---------------------");
+        console.warn("This is an error");
+    }
+
+    window.onload = Start;
+
+})();
